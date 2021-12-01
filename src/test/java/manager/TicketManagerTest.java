e60e2cc3a410e7d3fe108f6f4cfb355d86cf38da
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-01 14:37:30 +0500

    Задача 2 Comparator

--- src/test/java/manager/TicketManagerTest.java
+++ src/test/java/manager/TicketManagerTest.java
@@ -12,12 +12,12 @@ public class TicketManagerTest {
     TicketRepo repo = new TicketRepo();
     TicketManager manager = new TicketManager(repo);
 
-    Ticket offer1 = new Ticket(1, "MQF", "SWO", 120, 14000);
-    Ticket offer2 = new Ticket(2, "MQF", "SWO", 120, 14300);
-    Ticket offer3 = new Ticket(3, "SWO", "TEL", 120, 44500);
-    Ticket offer4 = new Ticket(4, "SWO", "TGD", 120, 32000);
-    Ticket offer5 = new Ticket(5, "TGD", "TEL", 120, 16000);
-    Ticket offer6 = new Ticket(6, "TGD", "TEL", 120, 17000);
+    Ticket offer1 = new Ticket(1, "MQF", "SWO", 122, 14500);
+    Ticket offer2 = new Ticket(2, "MQF", "SWO", 121, 14300);
+    Ticket offer3 = new Ticket(3, "SWO", "TEL", 325, 44500);
+    Ticket offer4 = new Ticket(4, "SWO", "TGD", 220, 32000);
+    Ticket offer5 = new Ticket(5, "TGD", "TEL", 230, 16000);
+    Ticket offer6 = new Ticket(6, "TGD", "TEL", 210, 17000);
 
     @BeforeEach
     public void added() {
@@ -34,6 +34,7 @@ public class TicketManagerTest {
         assertArrayEquals(manager.searchBy("SWO", "TEL"), new Ticket[]{offer3});
     }
 
+
     @Test
     public void shouldSearchTicketNone() {
         assertArrayEquals(manager.searchBy("MQF", "TEL"), new Ticket[]{});
@@ -42,12 +43,18 @@ public class TicketManagerTest {
 
     @Test
     public void shouldSearchTicketDuplicatedFlights1() {
-        assertArrayEquals(manager.searchBy("MQF", "SWO"), new Ticket[]{offer1, offer2});
+        assertArrayEquals(manager.searchBy("MQF", "SWO"), new Ticket[]{offer2, offer1});
     }
 
     @Test
     public void shouldSearchTicketDuplicatedFlights2() {
-        assertArrayEquals(manager.searchBy("TGD", "TEL"), new Ticket[]{offer5, offer6});
+        assertArrayEquals(manager.searchBy("TGD", "TEL"), new Ticket[]{offer6, offer5});
     }
+
+    @Test
+    public void shouldSearchTicketTransitLeastTime() {
+        assertArrayEquals(manager.searchBy("MQF", "SWO"), new Ticket[]{offer2, offer1});
+    }
+
 }
 
